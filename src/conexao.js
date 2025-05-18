aeeabff93100ef30c18ede0ad6131308fab8beed
Marcos Donaciano Teixeira Júnior <marcosdjunior@gmail.com>
2025-05-18 14:00:28 -0300

    Update conexao.js

--- src/conexao.js
+++ src/conexao.js
@@ -7,7 +7,12 @@ const knex = require('knex')({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        ssl: {
+            require: true,
+            rejectUnauthorized: true,
+            ca: process.env.DB_CA ?? ''
+        }
     }
 });
 
